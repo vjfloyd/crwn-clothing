d72e4a59ac51612fa207c1a65e72c65798222c32
victorrojasb <victor.rojas@nautilus.team>
2023-08-25 18:32:47 -0500

    user.context with reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -11,12 +11,36 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-export const USER_TYPE = {
+export const USER_ACTION = {
   CURRENT_USER: "CURRENT_USER",
 };
 
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION.CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default:
+      throw new Error("unhandled type,  " + type);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION.CURRENT_USER, payload: user });
+  };
 
   const value = { currentUser, setCurrentUser };
 
