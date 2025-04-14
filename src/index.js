454d36e018229278ce5e7e826d1e751dc3164454
victor <pe20002313@prosegur.com>
2025-04-14 18:32:34 -0500

    feat: add stripe integration

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux"
 import {persistor, store} from "./store/store";
 import {PersistGate} from "redux-persist/integration/react";
+import {Elements} from "@stripe/react-stripe-js";
+import {stripePromise} from "./utils/stripe/stripe.utils";
+
+// import { Elements } from "./elements";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -19,7 +24,9 @@ root.render(
     <Provider store={store}>
         {/*<PersistGate loading={null} persistor={persistor}>*/}
             <BrowserRouter>
-                <App />
+                <Elements stripe={stripePromise}>
+                    <App />
+                </Elements>
             </BrowserRouter>
         {/*</PersistGate>*/}
     </Provider>
